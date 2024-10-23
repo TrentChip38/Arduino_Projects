80ac8095734186dc3dea0cd84fbe1057f1c966b9
TrentChip38 <156025773+TrentChip38@users.noreply.github.com>
2024-10-22 22:08:23 -0600

    Add files via upload

---
+++ BattleBrosBot/BattleBrosBot.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // continuously monitor the input from reciever
+  // turn on motors when pins are on
+  // turn on light when turned on
+  // turn on another light if theres connection
+  
+}
